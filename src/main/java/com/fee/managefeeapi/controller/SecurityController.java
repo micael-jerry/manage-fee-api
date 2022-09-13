a3f26bfa71855b48f87c7fb26470e0d57d7b1880
Micael Jerry <hei.jerry.2@gmail.com>
2022-09-13 21:00:14 +0300

    fix: update readme

---
+++ src/main/java/com/fee/managefeeapi/controller/SecurityController.java
@@ -0,0 +1,21 @@
+package com.fee.managefeeapi.controller;
+
+import org.springframework.security.config.web.servlet.headers.HeadersSecurityMarker;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class SecurityController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "hello world";
+    }
+
+    @GetMapping(value = "/auth")
+    public Authentication authentication(@HeadersSecurityMarker Authentication authentication) {
+        return authentication;
+    }
+}
