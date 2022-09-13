8350c81648dc0ce86b49a8ccaf5c8c980ffaa183
Micael Jerry <hei.jerry.2@gmail.com>
2022-09-13 22:38:42 +0300

    fix: add get by lastname

--- src/main/java/com/fee/managefeeapi/repository/FeeRepository.java
+++ src/main/java/com/fee/managefeeapi/repository/FeeRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface FeeRepository extends JpaRepository<Fee,Integer> {
+public interface FeeRepository extends JpaRepository<Fee, Integer> {
     List<Fee> findAllByStudent_Id(int studentId);
+
     List<Fee> findAllBySchoolYear_Id(int schoolYearId);
+
+    List<Fee> findAllByStudentLastname(String lastname);
 }
